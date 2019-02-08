007953bf1b45c8628b8d2b9edc23db7ee62d7b2a
Daniel Rice <Daniel.L.Rice@gmail.com>
2019-02-08 15:31:29 +0000

    Add message.js.

---
+++ src/message.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class Message extends Component {
+  render() {
+    const { big, children: message } = this.props;
+    return big ? <h1>{message}</h1> : <h5>{message}</h5>;
+  }
+}
+
+class MessageApp extends Component {
+  render() {
+    return (
+      <div>
+        <Message big={true}>Good morning!</Message>
+        <Message big>Good day!</Message>
+        <Message>Good night!</Message>
+      </div>
+    );
+  }
+}
+
+export default MessageApp;
